904610149e76f6a427e07080840ee31e687e9bf5
Ana Maria <gomitaana@hotmail.com>
2020-02-11 17:46:41 -0600

    Cambios a migracion de usuario

--- control-acceso/database/migrations/2014_10_12_000000_create_users_table.php
+++ control-acceso/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('Usuario', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nombreClaveUsuario');
+            $table->string('emailusuario')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('passwordUsuario');
+            $table->string('tipoUsuario');
+            $table->string('statusUsuario');
             $table->rememberToken();
             $table->timestamps();
         });
